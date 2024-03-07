32a2a464295bbb3aa0ce0635722734a988e45456
kwabxx <93738804+kwabxx@users.noreply.github.com>
2024-03-07 09:54:33 -0500

    Add files via upload

---
+++ ex.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $("#faq").click(function() {
+        $("#FAQS").toggle();
+    });
+})
+
+
+$(document).ready(function(){
+    $("#CostOfNetflix").click(function() {
+        $("#costofnetflix").toggle();
+    });
+})
+
+$(document).ready(function(){
+    $("#WhereToWatch").click(function() {
+        $("#wheretowatch").toggle();
+    });
+})
+
+$(document).ready(function(){
+    $("#HowToCancel").click(function() {
+        $("#howtocancel").toggle();
+    });
+})
+
+
+$(document).ready(function(){
+    $("#WhatDevice").click(function() {
+        $("#whatdevice").toggle();
+    });
+})
+
+
+$(document).ready(function(){
+    $("#GoodForKids").click(function() {
+        $("#goodforkids").toggle();
+    });
+})
+
+
+
